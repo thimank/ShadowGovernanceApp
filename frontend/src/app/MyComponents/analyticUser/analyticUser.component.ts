23dd8852049dc2e18c7ea2ab7541f378e3991d36
thimank <63395545+thimank@users.noreply.github.com>
2022-03-03 20:47:20 +0530

    Update analyticUser.component.ts

--- frontend/src/app/MyComponents/analyticUser/analyticUser.component.ts
+++ frontend/src/app/MyComponents/analyticUser/analyticUser.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AnalyticUserService } from 'src/app/MyServices/analyticUser.service';
-import { EndUserService } from 'src/app/MyServices/endUser.service';
+import { ExportService } from 'src/app/MyServices/export.service';
 import { endUser } from '../endUser/endUser';
 import { taskDetails } from '../endUser/taskDetails';
 
@@ -14,30 +14,45 @@ export class AnalyticUserComponent implements OnInit {
 
   endusers: endUser[];
   localItem: any;
-  criteria: taskDetails = new taskDetails("","","","",false);
+  criteria: taskDetails = new taskDetails("", "", "", "", false);
+  loggedIn = false;
+  sessionItem: any;
 
-  constructor(private aservice:AnalyticUserService,private router: Router) { 
+  constructor(private aservice: AnalyticUserService, private eservice: ExportService, private router: Router) {
     this.localItem = localStorage.getItem("users");
     this.endusers = JSON.parse(this.localItem);
+
+    this.sessionItem = sessionStorage.getItem('rloggedIn');
+    this.loggedIn = JSON.parse(this.sessionItem);
+
+    if (this.loggedIn === false) {
+      this.router.navigate(['/analyticLogin'])
+    }
   }
 
   ngOnInit(): void {
     this.localItem = localStorage.getItem("users");
     this.endusers = JSON.parse(this.localItem);
   }
-  ngDoCheck():void{
+  ngDoCheck(): void {
     this.localItem = localStorage.getItem("users");
     this.endusers = JSON.parse(this.localItem);
   }
 
-  public createReport(): void{
-    this.criteria.fromDate=this.criteria.fromDate.toString();
-    this.criteria.toDate=this.criteria.toDate.toString();
-    this.aservice.endUserReport(this.criteria).then(x=> localStorage.setItem('users',JSON.stringify(x)));
+  public createReport(): void {
+    this.criteria.fromDate = this.criteria.fromDate.toString();
+    this.criteria.toDate = this.criteria.toDate.toString();
+    this.aservice.endUserReport(this.criteria).then(x => localStorage.setItem('users', JSON.stringify(x)));
   }
 
   public logout(): void {
+    this.loggedIn = false;
+    sessionStorage.setItem('rloggedIn', JSON.stringify(this.loggedIn));
     this.router.navigate(['/analyticLogin']);
+  }
+
+  public exportData(): void {
+    this.eservice.exportToExcel(this.endusers, "Report")
   }
 
 }
