c24b93f1d0021e9d0ace57cbe82052a24facdda2
thimank <63395545+thimank@users.noreply.github.com>
2022-03-03 20:45:29 +0530

    Added session login

--- frontend/src/app/MyComponents/admin/adminLogin.component.ts
+++ frontend/src/app/MyComponents/admin/adminLogin.component.ts
@@ -9,18 +9,29 @@ import { AdminService } from 'src/app/MyServices/admin.service';
 })
 export class AdminLoginComponent implements OnInit {
 
-  userData={id:0, password:"", loggedIn:false}
+  userData={id:0, password:""}
+  loggedIn = false;
+  sessionItem: any;
 
-  constructor(private aservice:AdminService,private router: Router) { this.userData; }
+  constructor(private aservice:AdminService,private router: Router) {
+    this.sessionItem = sessionStorage.getItem('aloggedIn');
+    this.loggedIn = JSON.parse(this.sessionItem);
+  }
 
-  ngOnInit(): void { this.userData; }
+  ngOnInit(): void { }
 
   public userlogin(): void{
     this.aservice.adminLogin(this.userData).then(x=>{
-      if(x==="Login Successful") {this.router.navigate(['/admin']); this.userData.loggedIn=true;}
-      else {alert("     LOGIN UNSUCCESSFUL!! \n      Check Your Credentials"); this.userData.loggedIn=false;}
+      if(x==="Login Successful") {
+        this.router.navigate(['/admin']); 
+        this.loggedIn=true;
+        sessionStorage.setItem('aloggedIn', JSON.stringify(this.loggedIn));
+      }
+      else {
+        alert("     LOGIN UNSUCCESSFUL!! \n      Check Your Credentials"); 
+        this.loggedIn=false;
+      }
     });
-    console.log(this.userData);
   }
 
 }
